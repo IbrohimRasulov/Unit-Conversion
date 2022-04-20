c0006b8f885632de60544edd413f1537d7e473d2
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-04-21 02:25:31 +0500

    Add condition to set value no zero when there is no input

--- index.js
+++ index.js
@@ -25,27 +25,53 @@ const fahrenheitLeft = document.querySelector('#id-fahrenheit-left');
 const celsiusRight = document.querySelector('#id-celsius-right');
 
 inputValue.addEventListener('input', () => {
-  //* Converted LENGTH
-  meterLeft.textContent = inputValue.value;
-  feetLeft.textContent = inputValue.value;
-  meterRight.textContent = Math.round((inputValue.value * 3.28084) * 100) / 100;
-  feetRight.textContent = Math.round((inputValue.value * 0.3048) * 100) / 100;
+  if (!inputValue.value == '') {
+    //* Converted LENGTH
+    meterLeft.textContent = inputValue.value;
+    feetLeft.textContent = inputValue.value;
+    meterRight.textContent = Math.round((inputValue.value * 3.28084) * 100) / 100;
+    feetRight.textContent = Math.round((inputValue.value * 0.3048) * 100) / 100;
 
-  //* Converted VOLUME
-  literLeft.textContent = inputValue.value;
-  gallonLeft.textContent = inputValue.value;
-  gallonRight.textContent = Math.round((inputValue.value * 0.264172) * 100) / 100;
-  literRight.textContent = Math.round((inputValue.value * 3.785) * 100) / 100;
+    //* Converted VOLUME
+    literLeft.textContent = inputValue.value;
+    gallonLeft.textContent = inputValue.value;
+    gallonRight.textContent = Math.round((inputValue.value * 0.264172) * 100) / 100;
+    literRight.textContent = Math.round((inputValue.value * 3.785) * 100) / 100;
 
-  //* Converted WEIGHT
-  kiloLeft.textContent = inputValue.value;
-  poundLeft.textContent = inputValue.value;
-  poundRight.textContent = Math.round((inputValue.value * 2.20462) * 100) / 100;
-  kiloRight.textContent = Math.round((inputValue.value * 0.453592) * 100) / 100;
+    //* Converted WEIGHT
+    kiloLeft.textContent = inputValue.value;
+    poundLeft.textContent = inputValue.value;
+    poundRight.textContent = Math.round((inputValue.value * 2.20462) * 100) / 100;
+    kiloRight.textContent = Math.round((inputValue.value * 0.453592) * 100) / 100;
 
-  //* Converted TEMPERATURE
-  celsiusLeft.textContent = inputValue.value;
-  fahrenheitLeft.textContent = inputValue.value;
-  fahrenheitRight.textContent = Math.round((inputValue.value * 2.20462) * 100) / 100;
-  celsiusRight.textContent = Math.round(((inputValue.value * 9/5) + 32) * 100) / 100;
+    //* Converted TEMPERATURE
+    celsiusLeft.textContent = inputValue.value;
+    fahrenheitLeft.textContent = inputValue.value;
+    fahrenheitRight.textContent = Math.round(((inputValue.value * 9 / 5) + 32) * 100) / 100;
+    celsiusRight.textContent = Math.round(((inputValue.value - 32) * 5 / 9) * 100) / 100;
+  } else {
+    //* Converted LENGTH
+    meterLeft.textContent = 0;
+    feetLeft.textContent = 0;
+    meterRight.textContent = 0;
+    feetRight.textContent = 0;
+
+    //* Converted VOLUME
+    literLeft.textContent = 0;
+    gallonLeft.textContent = 0;
+    gallonRight.textContent = 0;
+    literRight.textContent = 0;
+
+    //* Converted WEIGHT
+    kiloLeft.textContent = 0;
+    poundLeft.textContent = 0;
+    poundRight.textContent = 0;
+    kiloRight.textContent = 0;
+
+    //* Converted TEMPERATURE
+    celsiusLeft.textContent = 0;
+    fahrenheitLeft.textContent = 0;
+    fahrenheitRight.textContent = 0;
+    celsiusRight.textContent = 0;
+  }
 });
