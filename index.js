50d8c54ed952b0c90a68826e71ad9153ef5ba8a7
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-04-21 01:36:03 +0500

    Get access to spans

---
+++ index.js
@@ -0,0 +1,25 @@
+const inputValue = document.querySelector('input');
+
+//! Length
+const meterLeft = document.querySelector('#id-meter-left');
+const feetRight = document.querySelector('#id-feet-right');
+const feetLeft = document.querySelector('#id-feet-left');
+const meterRight = document.querySelector('#id-meter-right');
+
+//! Volume
+const literLeft = document.querySelector('#id-liter-left');
+const gallonRight = document.querySelector('#id-gallon-right');
+const gallonLeft = document.querySelector('#id-gallon-left');
+const literRight = document.querySelector('#id-liter-right');
+
+//! Weight
+const kiloLeft = document.querySelector('#id-kilo-left');
+const poundRight = document.querySelector('#id-pound-right');
+const poundLeft = document.querySelector('#id-pound-left');
+const kiloRight = document.querySelector('#id-kilo-right');
+
+//! Temperature
+const celsiusLeft = document.querySelector('#id-celsius-left');
+const fahrenheitRight = document.querySelector('#id-fahrenheit-right');
+const fahrenheitLeft = document.querySelector('#id-fahrenheit-left');
+const celsiusRight = document.querySelector('#id-celsius-right');
